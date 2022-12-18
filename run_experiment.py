56afa04d635f4d6b92d8ca3267f13f5400424990
tomyaacov <tomyaacov1210@gmail.com>
2022-12-18 10:09:12 +0200

    pn intersection

--- run_experiment.py
+++ run_experiment.py
@@ -14,9 +14,9 @@ for k, v in partitions.items():
 event_log = df_to_log(df)
 net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(event_log)
 fitness_token_based_replay = pm4py.fitness_token_based_replay(event_log, net, initial_marking, final_marking )
-fitness_alignments = pm4py.fitness_alignments(event_log, net, initial_marking, final_marking )
+#fitness_alignments = pm4py.fitness_alignments(event_log, net, initial_marking, final_marking )
 precision_token_based_replay = pm4py.precision_token_based_replay(event_log, net, initial_marking, final_marking )
-precision_alignments = pm4py.precision_alignments(event_log, net, initial_marking, final_marking )
+#precision_alignments = pm4py.precision_alignments(event_log, net, initial_marking, final_marking )
 generalization_evaluator_ = generalization_evaluator.apply(event_log, net, initial_marking, final_marking )
 simplicity_evaluator_ = simplicity_evaluator.apply(net)
 try:
@@ -26,18 +26,18 @@ except Exception as e:
 
 print("general model results:")
 print("fitness_token_based_replay:", fitness_token_based_replay)
-print("fitness_alignments:", fitness_alignments)
+#print("fitness_alignments:", fitness_alignments)
 print("precision_token_based_replay:", precision_token_based_replay)
-print("precision_alignments:", precision_alignments)
+#print("precision_alignments:", precision_alignments)
 print("generalization_evaluator:", generalization_evaluator_)
 print("simplicity_evaluator:", simplicity_evaluator_)
 
 results = {
-    "fitness_token_based_replay": [],
-    "fitness_alignments": [],
-    "precision_token_based_replay": [],
-    "precision_alignments": [],
-    "generalization_evaluator": [],
+#    "fitness_token_based_replay": [],
+#    "fitness_alignments": [],
+#    "precision_token_based_replay": [],
+#    "precision_alignments": [],
+#    "generalization_evaluator": [],
     "simplicity_evaluator": [],
 }
 pn_list = []
@@ -48,11 +48,11 @@ for p_name in partitions:
     net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(event_log)
     net, initial_marking, final_marking = get_modified_pn(net, events_map[p_name])
     event_log = df_to_log(df)
-    results["fitness_token_based_replay"].append(pm4py.fitness_token_based_replay(event_log, net, initial_marking, final_marking ))
-    results["fitness_alignments"].append(pm4py.fitness_alignments(event_log, net, initial_marking, final_marking ))
-    results["precision_token_based_replay"].append(pm4py.precision_token_based_replay(event_log, net, initial_marking, final_marking ))
-    results["precision_alignments"].append(pm4py.precision_alignments(event_log, net, initial_marking, final_marking ))
-    results["generalization_evaluator"].append(generalization_evaluator.apply(event_log, net, initial_marking, final_marking ))
+    # results["fitness_token_based_replay"].append(pm4py.fitness_token_based_replay(event_log, net, initial_marking, final_marking ))
+    # results["fitness_alignments"].append(pm4py.fitness_alignments(event_log, net, initial_marking, final_marking ))
+    # results["precision_token_based_replay"].append(pm4py.precision_token_based_replay(event_log, net, initial_marking, final_marking ))
+    # results["precision_alignments"].append(pm4py.precision_alignments(event_log, net, initial_marking, final_marking ))
+    # results["generalization_evaluator"].append(generalization_evaluator.apply(event_log, net, initial_marking, final_marking ))
     results["simplicity_evaluator"].append(simplicity_evaluator.apply(net))
     try:
         pm4py.write_pnml(net, initial_marking, final_marking, "output/ad14_model_" + p_name + ".pnml")
@@ -64,16 +64,16 @@ print("our model results:")
 print(results)
 net, initial_marking, final_marking = get_intersection_pn(pn_list)
 fitness_token_based_replay = pm4py.fitness_token_based_replay(event_log, net, initial_marking, final_marking )
-fitness_alignments = pm4py.fitness_alignments(event_log, net, initial_marking, final_marking )
+#fitness_alignments = pm4py.fitness_alignments(event_log, net, initial_marking, final_marking )
 precision_token_based_replay = pm4py.precision_token_based_replay(event_log, net, initial_marking, final_marking )
-precision_alignments = pm4py.precision_alignments(event_log, net, initial_marking, final_marking )
+#precision_alignments = pm4py.precision_alignments(event_log, net, initial_marking, final_marking )
 generalization_evaluator_ = generalization_evaluator.apply(event_log, net, initial_marking, final_marking )
 simplicity_evaluator_ = simplicity_evaluator.apply(net)
 
 print("intersected model results:")
 print("fitness_token_based_replay:", fitness_token_based_replay)
-print("fitness_alignments:", fitness_alignments)
+#print("fitness_alignments:", fitness_alignments)
 print("precision_token_based_replay:", precision_token_based_replay)
-print("precision_alignments:", precision_alignments)
+#print("precision_alignments:", precision_alignments)
 print("generalization_evaluator:", generalization_evaluator_)
 print("simplicity_evaluator:", simplicity_evaluator_)
