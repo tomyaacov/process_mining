cf42f197df5ea428de39cb75e1eb53a5728c0b1b
tomyaacov <tomyaacov1210@gmail.com>
2022-12-20 12:12:36 +0200

    connect4 traces

--- connect_4.py
+++ connect_4.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 import sys
@@ -30,6 +32,14 @@ def print_board(board):
     print(np.flip(board, 0))
 
 
+def is_game_over(board):
+    for c in range(COLUMN_COUNT):
+        if is_valid_location(board, c):
+            return False
+    return True
+
+
+
 def winning_move(board, piece):
     # Check horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
@@ -55,66 +65,42 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
 
+TRACES = set()
 
-board = create_board()
-print_board(board)
-game_over = False
-turn = 0
+while len(TRACES) < 300000:
+    t = []
+    board = create_board()
+    #print_board(board)
+    game_over = False
+    turn = 1
 
+    player_map = {1: "A", 2: "B"}
+    column_options = list(range(COLUMN_COUNT))
 
+    while not game_over:
+        while True:
+            col = random.choice(column_options)
+            if is_valid_location(board, col):
+                row = get_next_open_row(board, col)
+                drop_piece(board, row, col, turn)
+                t.append(player_map[turn] + str(row) + str(col))
+                if not is_valid_location(board, col):
+                    column_options.remove(col)
+                if winning_move(board, turn):
+                    game_over = True
+                if is_game_over(board):
+                    game_over = True
+                break
+        #print_board(board)
+        turn = 1 + (turn % 2)
 
-while not game_over:
+    TRACES.add(tuple(t))
+    if len(TRACES) % 10000 == 0:
+        print(len(TRACES))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-        if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-            posx = event.pos[0]
-            if turn == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
-            else:
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
-        pygame.display.update()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-            # print(event.pos)
-            # Ask for Player 1 Input
-            if turn == 0:
-                posx = event.pos[0]
-                col = int(math.floor(posx / SQUARESIZE))
-
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 1)
-
-                    if winning_move(board, 1):
-                        label = myfont.render("Player 1 wins!!", 1, RED)
-                        screen.blit(label, (40, 10))
-                        game_over = True
+import csv
+with open("data/connect4/connect4_traces.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerows(TRACES)
 
 
-            # # Ask for Player 2 Input
-            else:
-                posx = event.pos[0]
-                col = int(math.floor(posx / SQUARESIZE))
-
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 2)
-
-                    if winning_move(board, 2):
-                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
-                        screen.blit(label, (40, 10))
-                        game_over = True
-
-            print_board(board)
-            draw_board(board)
-
-            turn += 1
-            turn = turn % 2
-
-            if game_over:
-                pygame.time.wait(3000)
