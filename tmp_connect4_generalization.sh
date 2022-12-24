a15f3ffb4db11eda8d3047efbf148a4798cb7a5a
tomyaacov <tomyaacov1210@gmail.com>
2022-12-24 16:52:45 +0200

    running experiment

--- tmp_connect4_generalization.sh
+++ tmp_connect4_generalization.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 ##SBATCH --time 3-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name tmp_connect4 ### name of the job. replace my_job with your desired job name
-#SBATCH --output tmp_connect4.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name tmp_connect4_generalization ### name of the job. replace my_job with your desired job name
+#SBATCH --output tmp_connect4_generalization.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications � replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=32G ### total amount of RAM
@@ -14,5 +14,5 @@
 module load anaconda ### load anaconda module
 source activate process_mining
 cd ~/repos/process_mining/
-/home/tomya/.conda/envs/process_mining/bin/python tmp_connect4.py
+/home/tomya/.conda/envs/process_mining/bin/python tmp_connect4_generalization.py
 
